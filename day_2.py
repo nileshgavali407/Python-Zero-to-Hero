5aece075a52ef7dc083e3b84e3f6e13a85e000f7
biswajit paloi <bpaloi5555@gmail.com>
2022-02-01 23:15:07 +0530

    day 2\

---
+++ day_2.py
@@ -0,0 +1,12 @@
+
+
+lottery  = "hello coders"
+# 'h' or 'e' or 'o' or 'c' or 'd'
+
+print("choose correct character of the word '",lottery,"' to win the lottery")
+inputs = input(" ").lower()
+
+if inputs == "h" or inputs == "e" or inputs == "o" or inputs == "c" or inputs == "d":
+    print("you win thwe lottery")
+else:
+    print("sorry")
