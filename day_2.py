8f984c93e6c525e102c3c9ea1fe7537fbbbafc3f
biswajit paloi <bpaloi5555@gmail.com>
2022-02-02 11:59:12 +0530

    day 2

--- day_2.py
+++ day_2.py
@@ -1,9 +1,9 @@
 lottery  = "hello coders"
 # 'h' or 'e' or 'o' or 'c' or 'd'
 print("choose correct character of the word '",lottery,"' to win the lottery")
-inputs = input(" ").lower()
+inputs = input(" ")
 
-if inputs == lottery[0] or inputs == lottery[1] or inputs == lottery[4] or inputs == lottery[6] or inputs == lottery[8]:
+if inputs == lottery[0].lower() or inputs == lottery[1].lower() or inputs == lottery[4].lower() or inputs == lottery[6].lower() or inputs == lottery[8].lower():
     print("congratulation,you win thwe lottery")
 else:
     print("sorry, You didn't win the lottery")
