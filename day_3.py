28d937a85d389b3a6cb2014e7801fb7525a8283e
biswajit paloi <bpaloi5555@gmail.com>
2022-02-02 21:04:14 +0530

    day 3 done

--- day_3.py
+++ day_3.py
@@ -1,3 +1,6 @@
+# qustion: From a dict, how can we print the Key whose value is maximum ??
+
+
 Tv = {'BreakingBad':100, 'GameOfThrones':12, 'mirzapur' : 88}
  
 Keymax = max(Tv, key= lambda x: Tv[x])
